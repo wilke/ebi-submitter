e5e10125d62601c98d913db7173dde88112a8e0a
Andreas Wilke <wilke@mcs.anl.gov>
2017-05-05 14:50:35 -0500

    Create list from root element down

--- scripts/getNCBImetagenomeTaxonomy.pl
+++ scripts/getNCBImetagenomeTaxonomy.pl
@@ -18,6 +18,8 @@ use XML::LibXML;
 # http://www.ebi.ac.uk/ena/data/view/Taxon:410657&display=xml&download=xml&filename=410657.xml for non-host-associated.
 # http://www.ebi.ac.uk/ena/data/view/Taxon:410656&display=xml&download=xml&filename=410656.xml for host-associated.
 
+
+my $turl  = 'http://www.ebi.ac.uk/ena/data/view/Taxon:408169&display=xml&download=xml&filename=408169.xml' ;
 my $nhurl = "http://www.ebi.ac.uk/ena/data/view/Taxon:410657&display=xml&download=xml&filename=410657.xml" ;
 my $hurl  = "http://www.ebi.ac.uk/ena/data/view/Taxon:410656&display=xml&download=xml&filename=410656.xml" ;
 
@@ -25,12 +27,85 @@ my $hurl  = "http://www.ebi.ac.uk/ena/data/view/Taxon:410656&display=xml&downloa
 my $ua = LWP::UserAgent->new;
 $ua->agent('NCBI Taxonomy Client 0.1');
 
-# xml parser
- #my $xs = XML::Simple->new(ForceArray => 1, KeepRoot => 1);
-my $type = 'non-host-associated' ;
 
-foreach my $url ( $nhurl ,$hurl ) {
-    
+# get top level
+my $response = $ua->get($turl);
+
+unless ( $response->is_success ) {
+    print STDERR "Error retrieving data\n";
+    print STDERR $response->status_line, "\n";
+    exit;
+}
+
+my $dom = XML::LibXML->load_xml(string => $response->content );
+my $root = $dom->documentElement ;
+my $type = 'none' ;
+
+foreach my $node ($root->findnodes('//children/taxon') ) {
+  my @attribs = $node->attributes();
+  #print "Value:\t" , $attribs[0]->value , "\t", $attribs[1]->value , "\n" ;
+  my $tmp1 = $attribs[0]->value ;
+  my $tmp2 = $attribs[1]->value ;
+  # my ($name)  = $attribs[0]->value =~ /scientificName="([\w\s]+) metagenome"/ ;
+  # my ($taxid) = $attribs[1]->value =~ /taxId="(\d+)"/ ;
+  my ($name)  = $attribs[0]->value =~ /([\w\s]+) metagenome/ ;
+  my ($taxid) = $attribs[1]->value =~ /(\d+)/ ;
+  #print join ("\t" , ($name || $attribs[0]->value || 'error') , 'unspecified' , $taxid || 'error' ) , "\n" ;
+  print join ("\t" , ($attribs[0]->value || 'error') , 'unspecified' , $taxid || 'error' ) , "\n" ;
+  &get_nodes( (undef || $attribs[0]->value || 'error') , $taxid , $ua) ;
+}
+exit ;
+
+
+# # xml parser
+#  #my $xs = XML::Simple->new(ForceArray => 1, KeepRoot => 1);
+# my $type = 'non-host-associated' ;
+#
+# foreach my $url ( $nhurl ,$hurl ) {
+#
+#   my $response = $ua->get($url);
+#
+#   unless ( $response->is_success ) {
+#       print STDERR "Error retrieving data\n";
+#       print STDERR $response->status_line, "\n";
+#       exit;
+#   }
+#
+#
+#
+#   my $dom = XML::LibXML->load_xml(string => $response->content );
+#   my $root = $dom->documentElement ;
+#
+#
+#
+#
+#   # '//foo[@bar="baz"][position()<4]'
+#
+#
+#   foreach my $node ($root->findnodes('//children/taxon') ) {
+#     #print join "\t" , "Found:" , $node->nodeName , ($node->nodeValue || "none") , $node->textContent , ($node->attributes) , "\n" ;
+#
+#     my @attribs = $node->attributes();
+#     #print "Value:\t" , $attribs[0]->value , "\t", $attribs[1]->value , "\n" ;
+#     my $tmp1 = $attribs[0]->value ;
+#     my $tmp2 = $attribs[1]->value ;
+#     # my ($name)  = $attribs[0]->value =~ /scientificName="([\w\s]+) metagenome"/ ;
+#     # my ($taxid) = $attribs[1]->value =~ /taxId="(\d+)"/ ;
+#     my ($name)  = $attribs[0]->value =~ /([\w\s]+) metagenome/ ;
+#     my ($taxid) = $attribs[1]->value =~ /(\d+)/ ;
+#     print join ("\t" , $type , ($name || $attribs[0]->value || 'error') , $taxid || 'error' ) , "\n" ;
+#   }
+#
+#   $type = 'host-associated' ;
+#
+#
+# }
+
+
+sub get_nodes {
+  my ($type, $taxid , $ua) = @_ ;
+  
+  my $url = 'http://www.ebi.ac.uk/ena/data/view/Taxon:'.$taxid.'&display=xml&download=xml&filename='.$taxid.'.xml' ;
   my $response = $ua->get($url);
   
   unless ( $response->is_success ) {
@@ -39,29 +114,23 @@ foreach my $url ( $nhurl ,$hurl ) {
       exit;
   }
   
-
-  
   my $dom = XML::LibXML->load_xml(string => $response->content );
   my $root = $dom->documentElement ;
- 
-  
-
-
-  # '//foo[@bar="baz"][position()<4]' 
-
   
   foreach my $node ($root->findnodes('//children/taxon') ) {
-    #print join "\t" , "Found:" , $node->nodeName , ($node->nodeValue || "none") , $node->textContent , $node->attributes , "\n" ;
-    my @attribs = $node->attributes();
-    my ($name) = $attribs[0] =~ /scientificName="([\w\s]+) metagenome"/ ;
-    my ($taxid) = $attribs[1] =~ /taxId="(\d+)"/ ;
-    print join ("\t" , $type , ($name || $attribs[0]) , $taxid ) , "\n" ;
-  }
-
-  $type = 'host-associated' ;
+    #print join "\t" , "Found:" , $node->nodeName , ($node->nodeValue || "none") , $node->textContent , ($node->attributes) , "\n" ;
   
+    my @attribs = $node->attributes();
+    #print "Value:\t" , $attribs[0]->value , "\t", $attribs[1]->value , "\n" ;
+    my $tmp1 = $attribs[0]->value ;
+    my $tmp2 = $attribs[1]->value ;
+    # my ($name)  = $attribs[0]->value =~ /scientificName="([\w\s]+) metagenome"/ ;
+    # my ($taxid) = $attribs[1]->value =~ /taxId="(\d+)"/ ;
+    my ($name)  = $attribs[0]->value =~ /([\w\s]+) metagenome/ ;
+    my ($taxid) = $attribs[1]->value =~ /(\d+)/ ;
+    print join ("\t" , $type , ($name || $attribs[0]->value || 'error') , $taxid || 'error' ) , "\n" ;
+  }
   
 }
-
 
 
